5146c0b446369eff30b09cf3bdd4aefca24a829a
ClayonStealth <claytonses@yahoo.com>
2023-12-27 23:48:08 -0600

    email is working and sending

--- src/Rsvp.jsx
+++ src/Rsvp.jsx
@@ -14,14 +14,15 @@ export default function Rsvp(props) {
 
     emailjs
       .sendForm(
-        "service_chwkmbo",
-        "template_9p2imej",
+        process.env.REACT_APP_YOUR_SERVICE_ID,
+        process.env.REACT_APP_YOUR_TEMPLATE_ID,
         form.current,
-        "NHoQwlFQpc2euGO3O"
+        process.env.REACT_APP_YOUR_PUBLIC_KEY
       )
       .then(
         (result) => {
           console.log(result.text);
+          console.log("Success");
         },
         (error) => {
           console.log(error.text);
@@ -31,70 +32,71 @@ export default function Rsvp(props) {
 
   return (
     <div
-      method='post'
-      name='rsvp'
-      className=' w-full min-h-screen flex items-center justify-center bg-fixed bg-gradient-to-b from-white to-black'
+      method="post"
+      name="rsvp"
+      className=" w-full min-h-screen flex items-center justify-center bg-fixed bg-gradient-to-b from-white to-black"
     >
       {/* RSVP Form */}
       <form
         ref={form}
         onSubmit={sendEmail}
-        className='max-w-[400px] w-full mx-auto shadow-2xl p-8 rounded-lg'
+        className="max-w-[400px] w-full mx-auto shadow-2xl p-8 rounded-lg"
       >
-        <div className='flex flex-col mb-4 '>
+        <div className="flex flex-col mb-4 ">
           <input
-            className='bg-gray-200 p-2 rounded-2xl shadow-2xl '
-            type='text'
-            placeholder='First Name'
-            name='user_fName'
+            className="bg-gray-200 p-2 rounded-2xl shadow-2xl "
+            type="text"
+            placeholder="First Name"
+            name="user_fName"
             onChange={(e) => {
               setFName(e.target.value);
             }}
           />
         </div>
-        <div className='flex flex-col mb-4 '>
+        <div className="flex flex-col mb-4 ">
           <input
-            className='bg-gray-200 p-2 rounded-2xl shadow-2xl '
-            type='text'
-            placeholder='Last Name'
-            name='user_lName'
+            className="bg-gray-200 p-2 rounded-2xl shadow-2xl "
+            type="text"
+            placeholder="Last Name"
+            name="user_lName"
             onChange={(e) => {
               setLName(e.target.value);
             }}
           />
         </div>
-        <div className='flex flex-col mb-4'>
+        <div className="flex flex-col mb-4">
           <input
-            className='bg-gray-200 p-2 rounded-2xl shadow-2xl'
-            type='email'
-            placeholder='Email'
-            name='user_email'
+            className="bg-gray-200 p-2 rounded-2xl shadow-2xl"
+            type="email"
+            placeholder="Email"
+            name="user_email"
             onChange={(e) => {
               setEmail(e.target.value);
             }}
           />
         </div>
-        <div className='flex flex-col'>
+        <div className="flex flex-col">
           <input
-            className='p-2 rounded-2xl shadow-2xl bg-gray-200'
-            type='text'
-            placeholder='Affiliation'
-            name='user_affiliation'
+            className="p-2 rounded-2xl shadow-2xl bg-gray-200"
+            type="text"
+            placeholder="Affiliation"
+            name="user_affiliation"
             onChange={(e) => {
               setAffiliation(e.target.value);
             }}
           />
         </div>
-        <Link to='thank-you' smooth={true} duration={1000}>
+        <Link to="thank-you" smooth={true} duration={1000}>
           <button
-            type='submit'
-            className='w-full py-3 mt-8 bg-black text-white rounded-md'
+            type="submit"
+            className="w-full py-3 mt-8 bg-black text-white rounded-md"
             onClick={(e) => {
               e.preventDefault();
               props.setTyMessage(`Thank you ${fname} for RSVPing`);
               props.setAtlasMsg(
                 "A member of the Atlas team will reach out to you shortly."
               );
+              sendEmail(e);
             }}
           >
             RSVP NOW
